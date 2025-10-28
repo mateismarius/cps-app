cca261ef116794eb5ed3de49941b3907adebf1f7
mateismarius <103111532+mateismarius@users.noreply.github.com>
2025-10-28 08:24:25 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,10 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
-    })
 
         // Register Spatie Permission middleware aliases
         $middleware->alias([
