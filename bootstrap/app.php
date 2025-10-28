a209a1bd54996019604e7da61a6053b51f90110c
mateismarius <103111532+mateismarius@users.noreply.github.com>
2025-10-28 08:21:17 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
+
         // Register Spatie Permission middleware aliases
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
